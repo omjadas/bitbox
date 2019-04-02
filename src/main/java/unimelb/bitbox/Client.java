deff1a33a651bb06820a84d9ec9581f380e63518
Vishal Egbert <vishal_egbert@hotmail.com>
2019-04-02 23:38:17 +1100

    Added basic Client class structure and functions

---
+++ src/main/java/unimelb/bitbox/Client.java
@@ -0,0 +1,33 @@
+package unimelb.bitbox;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client {
+	public static ArrayList<Client> establishedClients;
+	private Socket clientSocket;
+	
+	public Client(String address, int port) {
+		try {
+			this.clientSocket = new Socket(address, port);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Client(Socket socket) {
+		
+	}
+	
+	
+	public void initiateHandshake() {
+		
+	}
+	
+	public void respondToHandshake() {
+		
+	}
+}
--- src/main/java/unimelb/bitbox/Peer.java
+++ src/main/java/unimelb/bitbox/Peer.java
@@ -1,14 +1,18 @@
 package unimelb.bitbox;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
 
+
 import unimelb.bitbox.util.Configuration;
 
-public class Peer 
-{
+public class Peer extends Thread{
 	private static Logger log = Logger.getLogger(Peer.class.getName());
+	private ServerSocket serverSocket;
+	
+	
     public static void main( String[] args ) throws IOException, NumberFormatException, NoSuchAlgorithmException
     {
     	System.setProperty("java.util.logging.SimpleFormatter.format",
@@ -18,5 +22,19 @@ public class Peer
         
         new ServerMain();
         
+        //new Client("127.0.0.1", 3467);        
     }
+    
+    public void run () {
+    	try {
+			serverSocket = new ServerSocket(Integer.parseInt(Configuration.getConfigurationValue("port")));
+			
+			while (true) {
+	    		new Client(serverSocket.accept());
+	    	}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    }    
 }
