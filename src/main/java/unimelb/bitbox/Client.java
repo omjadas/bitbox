0afb003f0bc7cd6857685847aa5bcaba6a8ff5db
omjadas <omja.das@gmail.com>
2019-04-03 18:11:39 +1100

    formatted code and converted tabs to spaces

--- src/main/java/unimelb/bitbox/Client.java
+++ src/main/java/unimelb/bitbox/Client.java
@@ -5,29 +5,27 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Client {
-	public static ArrayList<Client> establishedClients;
-	private Socket clientSocket;
-	
-	public Client(String address, int port) {
-		try {
-			this.clientSocket = new Socket(address, port);
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public Client(Socket socket) {
-		
-	}
-	
-	
-	public void initiateHandshake() {
-		
-	}
-	
-	public void respondToHandshake() {
-		
-	}
+    public static ArrayList<Client> establishedClients;
+    private Socket clientSocket;
+
+    public Client(String address, int port) {
+        try {
+            this.clientSocket = new Socket(address, port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Client(Socket socket) {
+
+    }
+
+    public void initiateHandshake() {
+
+    }
+
+    public void respondToHandshake() {
+
+    }
 }
