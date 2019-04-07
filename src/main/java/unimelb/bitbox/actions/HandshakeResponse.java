7514d02ff01c5e8bea10c77feaa569d7cf8cef34
omjadas <omja.das@gmail.com>
2019-04-07 20:35:44 +1000

    Added HandshakeResponse variables

--- src/main/java/unimelb/bitbox/actions/HandshakeResponse.java
+++ src/main/java/unimelb/bitbox/actions/HandshakeResponse.java
@@ -6,9 +6,13 @@ public class HandshakeResponse implements Action {
 
     private Socket socket;
     private static final String command = "HANDSHAKE_RESPONSE";
+    private String host;
+    private int port;
 
-    public HandshakeResponse(Socket socket) {
+    public HandshakeResponse(Socket socket, String host, int port) {
         this.socket = socket;
+        this.host = host;
+        this.port = port;
     }
 
     @Override
