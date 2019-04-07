557da225eb4130b9f4e66a491b6b1dee2b7cc462
omjadas <omja.das@gmail.com>
2019-04-07 21:18:43 +1000

    Added toJSON method in InvalidProtocol

--- src/main/java/unimelb/bitbox/actions/InvalidProtocol.java
+++ src/main/java/unimelb/bitbox/actions/InvalidProtocol.java
@@ -1,6 +1,7 @@
 package unimelb.bitbox.actions;
 
 import java.net.Socket;
+import unimelb.bitbox.util.Document;
 
 public class InvalidProtocol implements Action {
 
@@ -26,6 +27,15 @@ public class InvalidProtocol implements Action {
     @Override
     public void send() {
 
+    }
+
+    private String toJSON() {
+        Document message = new Document();
+
+        message.append("command", command);
+        message.append("message", message);
+
+        return message.toJson();
     }
 
 }
