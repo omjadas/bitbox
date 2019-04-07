053851f048b25dbddb812f2e3b54fb819888836f
omjadas <omja.das@gmail.com>
2019-04-07 20:34:54 +1000

    Added HandshakeRequest variables

--- src/main/java/unimelb/bitbox/actions/HandshakeRequest.java
+++ src/main/java/unimelb/bitbox/actions/HandshakeRequest.java
@@ -6,9 +6,13 @@ public class HandshakeRequest implements Action {
 
     private Socket socket;
     private static final String command = "HANDSHAKE_REQUEST";
+    private String host;
+    private int port;
 
-    public HandshakeRequest(Socket socket) {
+    public HandshakeRequest(Socket socket, String host, int port) {
         this.socket = socket;
+        this.host = host;
+        this.port = port;
     }
 
     @Override
