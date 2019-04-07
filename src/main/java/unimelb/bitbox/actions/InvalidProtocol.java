e9194b249fbfe0019b55fca75ab7928fe735350c
omjadas <omja.das@gmail.com>
2019-04-07 16:34:31 +1000

    Added InvalidProtocol skeleton

--- src/main/java/unimelb/bitbox/actions/InvalidProtocol.java
+++ src/main/java/unimelb/bitbox/actions/InvalidProtocol.java
@@ -1,1 +1,15 @@
 package unimelb.bitbox.actions;
+
+public class InvalidProtocol implements Action {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public int compare(Action action) {
+        return 0;
+    }
+
+}
