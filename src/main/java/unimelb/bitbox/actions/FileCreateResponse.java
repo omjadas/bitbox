1fdd17e0dac8d1b1907c81d07264f250f013896a
omjadas <omja.das@gmail.com>
2019-04-07 20:29:18 +1000

    Added FileCreateResponse variables

--- src/main/java/unimelb/bitbox/actions/FileCreateResponse.java
+++ src/main/java/unimelb/bitbox/actions/FileCreateResponse.java
@@ -7,10 +7,12 @@ public class FileCreateResponse implements Action {
 
     private Socket socket;
     private static final String command = "FILE_CREATE_RESPONSE";
+    private FileDescriptor fileDescriptor;
     private String pathName;
 
-    public FileCreateResponse(Socket socket, String pathName) {
+    public FileCreateResponse(Socket socket, FileDescriptor fileDescriptor, String pathName) {
         this.socket = socket;
+        this.fileDescriptor = fileDescriptor;
         this.pathName = pathName;
     }
 
