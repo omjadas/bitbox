10d818e6fb36d01e82206e6ea1b100472ec02a17
omjadas <omja.das@gmail.com>
2019-04-07 19:57:49 +1000

    Added FileBytesRequest variables

--- src/main/java/unimelb/bitbox/actions/FileBytesRequest.java
+++ src/main/java/unimelb/bitbox/actions/FileBytesRequest.java
@@ -7,10 +7,14 @@ public class FileBytesRequest implements Action {
     private Socket socket;
     private static final String command = "FILE_BYTES_REQUEST";
     private String pathName;
+    private int position;
+    private int length;
 
-    public FileBytesRequest(Socket socket, String pathName) {
+    public FileBytesRequest(Socket socket, String pathName, int position, int length) {
         this.socket = socket;
         this.pathName = pathName;
+        this.position = position;
+        this.length = length;
     }
 
     @Override
