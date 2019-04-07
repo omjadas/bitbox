9fd7fcdd059175d0fc2eab556e8f77dfff521914
omjadas <omja.das@gmail.com>
2019-04-07 16:33:02 +1000

    Added FileModify skeleton

--- src/main/java/unimelb/bitbox/actions/FileModify.java
+++ src/main/java/unimelb/bitbox/actions/FileModify.java
@@ -1,1 +1,15 @@
 package unimelb.bitbox.actions;
+
+public class FileModify implements Action {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public int compare(Action action) {
+        return 0;
+    }
+
+}
