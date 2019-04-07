4b9fce67585fdeb16ee596cabd4d8e18c9a64f8e
omjadas <omja.das@gmail.com>
2019-04-07 16:31:30 +1000

    Added FileCreate skeleton

--- src/main/java/unimelb/bitbox/actions/FileCreate.java
+++ src/main/java/unimelb/bitbox/actions/FileCreate.java
@@ -1,1 +1,15 @@
 package unimelb.bitbox.actions;
+
+public class FileCreate implements Action {
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public int compare(Action action) {
+        return 0;
+    }
+
+}
