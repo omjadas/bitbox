bc55f5b7cec3f4eeda0f5213a571d56a8ee9111d
Jang <48899911+ehdgur9303@users.noreply.github.com>
2019-05-25 12:34:29 +1000

    command line parser

---
+++ src/main/java/unimelb/bitbox/Client.java
@@ -0,0 +1,56 @@
+package unimelb.bitbox;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import unimelb.bitbox.util.CommandLineArgs;
+
+public class Client {
+
+    public static void main(String[] args) {
+        //Object that will store the parsed command line arguments
+        CommandLineArgs argsBean = new CommandLineArgs();
+        
+        //Parser provided by args4j
+        CmdLineParser parser = new CmdLineParser(argsBean);
+        try {
+            
+            //Parse the arguments
+            parser.parseArgument(args);
+            
+            String command = argsBean.getCommand();
+            String server = argsBean.getServer();
+            int serverPort = argsBean.getServerPort();
+            
+             
+            
+            //After parsing, the fields in argsBean have been updated with the given
+            //command line arguments
+            
+            System.out.println("Command: " + command);
+            System.out.println("Server: " + server);
+            System.out.println("Server Port: " + serverPort);
+           
+            if (!command.equals("list_peers")) {
+                
+                String peer = argsBean.getPeer();
+                int peerPort = argsBean.getPeerPort();
+                
+                System.out.println("Peer: " + peer);
+                System.out.println("Peer Port: " + peerPort);
+            
+            }
+            
+        } catch (CmdLineException e) {
+            
+            System.err.println(e.getMessage());
+            
+            //Print the usage to help the user understand the arguments expected
+            //by the program
+            parser.printUsage(System.err);
+        }
+    
+    }
+    
+    
+
+}
---
+++ src/main/java/unimelb/bitbox/util/CommandLineArgs.java
@@ -0,0 +1,51 @@
+package unimelb.bitbox.util;
+
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.CmdLineException;
+
+public class CommandLineArgs {
+    
+    private String command;
+    
+    @Option(required = true, name = "-s", usage = "Server")
+    private String server;
+    
+    @Option(required = false, name = "-p", usage = "Peer")
+    private String peer;
+    
+    @Option(required = true, name = "-c", usage = "Command")
+    private void setCommand(String command) throws CmdLineException {
+        if (command.equals("list_peers") || command.equals("connect_peer") || command.equals("disconnect_peer")) {
+            this.command = command;
+        } else {
+            throw new CmdLineException(null, "Command is not recognised", null);
+        }
+    }
+    
+    public String getCommand() throws CmdLineException {
+        
+        if (!command.equals("list_peers") && this.peer == null) {
+            throw new CmdLineException(null, "Peer must be provided", null);
+        }
+        
+        return command;
+    }
+    
+    public String getServer()  {
+        return server.split(":")[0];
+    }
+    
+    public int getServerPort() {
+        return Integer.parseInt(server.split(":")[1]);   
+    }
+    
+    public String getPeer() {
+        return peer.split(":")[0];
+    }
+    
+    public int getPeerPort() {
+        return Integer.parseInt(peer.split(":")[1]);
+    }
+    
+    
+}
