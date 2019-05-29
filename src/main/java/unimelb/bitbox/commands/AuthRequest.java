20f3cd3d3854478b5866ffb1ef5e091184dae401
omjadas <omja.das@gmail.com>
2019-05-29 19:20:03 +1000

    implemented AuthRequest class

--- src/main/java/unimelb/bitbox/commands/AuthRequest.java
+++ src/main/java/unimelb/bitbox/commands/AuthRequest.java
@@ -1,6 +1,14 @@
 package unimelb.bitbox.commands;
 
+import unimelb.bitbox.util.Document;
+
 public class AuthRequest implements Command {
+    private static final String command = "AUTH_REQUEST";
+    private String identity;
+
+    public AuthRequest(String identity) {
+        this.identity = identity;
+    }
 
     @Override
     public void execute() {
@@ -8,8 +16,10 @@ public class AuthRequest implements Command {
     }
 
     @Override
-    public void send() {
-
+    public String getPayload() {
+        Document payload = new Document();
+        payload.append("command", command);
+        payload.append("identity", this.identity);
+        return payload.toJson();
     }
-
 }
